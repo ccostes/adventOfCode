886f190fcc1e1ee0266ccdb73775806165b6eb33
Chris Costes <costes.c@gmail.com>
2018-12-17 22:23:41 -0500

    update day 1 to find repeated value

--- 2018/1/add.go
+++ 2018/1/add.go
@@ -1,3 +1,9 @@
+/*
+	Input: 	text file containing one integer per line
+	Output: sum of all lines, along with the first repeated 
+			intermediate value
+*/
+
 package main
 	
 import (
@@ -18,21 +24,48 @@ func main(){
 	defer file.Close()
 
 	// use scanner to read file line by line and add to sum
-	var sum int64 = 0
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		// fmt.Println(scanner.Text())
-		i, err := strconv.ParseInt(scanner.Text(), 0, 64)
-		if err != nil {
-			log.Fatal(err)
+	var sum, repeat int64 = 0, 0	// intermediate value and repeated value
+	var foundRepeat bool = false
+	var m map[int64]int
+
+	m = make(map[int64]int)
+
+	for i := 0; !foundRepeat && i < 1000; i += 1 {
+		scanner := bufio.NewScanner(file)
+		for scanner.Scan() {
+			// fmt.Println(scanner.Text())
+			i, err := strconv.ParseInt(scanner.Text(), 0, 64)
+			if err != nil {
+				log.Fatal(err)
+			}
+			// calculate new intermediate value
+			sum += i
+			// fmt.Printf("%d ",sum)
+
+			// try to add it to the map and see if it's a repeat
+			if !foundRepeat {
+				_, present := m[sum]
+				// fmt.Printf("present: %v\n", present)
+				if present {
+					// found repeat
+					foundRepeat = true
+					repeat = sum
+				} else {
+					// add new value to the map
+					m[sum] = 1
+				}
+			}
 		}
-		
-		sum += i
+		// reset file to beginnnig if we haven't found anything
+		file.Seek(0,0)
 	}
 
-	if err := scanner.Err(); err != nil {
-		log.Fatal(err)
-	}
+	// fmt.Println(m)
 
-	fmt.Printf("Output: %d\n", sum)
+	fmt.Printf("Sum: %d\n", sum)
+	if foundRepeat {
+		fmt.Printf("Repeat: %d\n", repeat)
+	} else {
+		fmt.Println("No repeat\n")
+	}
 }
